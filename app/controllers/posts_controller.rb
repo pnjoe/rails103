0e3c4aeb223e3914cd0e985271a0d83fdb0a2869
陈为林 <guangdongjoe@gmail.cm>
2017-01-03 00:49:58 +0800

    终于搞定文章的编辑跟删除按钮了。内牛满面

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,11 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
   def create
     @group = Group.find(params[:group_id])
     @post = Post.new(post_params)
@@ -19,6 +24,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def update
+    #@group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to account_posts_path, notic: "Update success"
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: 'Content deleted'
+  end
 private
   def post_params
     params.require(:post).permit(:content)
