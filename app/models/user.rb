6174cab2db98d2bb22b9e15b78b3ba454cc0865a
陈为林 <guangdongjoe@gmail.cm>
2017-01-02 22:18:18 +0800

    join or quit group

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,12 @@ class User < ApplicationRecord
   def is_member_of?(group)
     participated_groups.include?(group)
   end
-  
+
+  def join!(group)
+    participated_group << group
+  end
+
+  def quit!(group)
+    participated_group.delete(group)
+  end
 end
